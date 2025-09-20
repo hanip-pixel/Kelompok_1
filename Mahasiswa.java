ff45cb095df96600a5b3038eadeaf3d707176391
hanip-pixel <hanifindramayou@gmail.com>
2025-09-20 08:34:24 +0700

    Code Mahasiswa.java

---
+++ Mahasiswa.java
@@ -0,0 +1,12 @@
+public class Mahasiswa {
+    String namaMahasiswa;
+    int nimMahasiswa;
+
+    void melaksanakanAbsensi(){
+        System.out.println("PBO");
+    }
+    
+    public String mengerjakanTugas(){
+        return "mahasiswa mengerjakan tugas";
+    }
+}
